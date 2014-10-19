3daf1360018b7653f37c43d350c7ddde91b421be
Sungjin <mac@macs-MacBook-Air.local>
2014-10-19 03:29:41 -0700

    update

--- compile.sh
+++
@@ -1,3 +0,0 @@
-#/bin/sh!
-#g++ -orun -std=c++11 -Ofast -march=native -funroll-loops main.cc -lpthread -larmadillo
-g++ -orun -std=c++11 -Ofast -march=native -funroll-loops main.cc -lpthread -larmadillo
